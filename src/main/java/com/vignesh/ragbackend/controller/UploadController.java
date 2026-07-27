e5fee380d417649591eaf317b69c3621b2063b65
Vicky <vigneshwara4011.com>
2026-07-27 20:45:20 +0530

    Extract text from uploaded PDF using PDFBox

--- src/main/java/com/vignesh/ragbackend/controller/UploadController.java
+++ src/main/java/com/vignesh/ragbackend/controller/UploadController.java
@@ -1,19 +1,36 @@
 package com.vignesh.ragbackend.controller;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class UploadController {
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadPdf(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> uploadPdf(
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
 
-        System.out.println("Received File : " + file.getOriginalFilename());
+        PDDocument document = Loader.loadPDF(file.getBytes());
 
-        return ResponseEntity.ok("Upload Successful");
+        PDFTextStripper stripper = new PDFTextStripper();
+
+        String text = stripper.getText(document);
+
+        document.close();
+
+        System.out.println("=================================");
+        System.out.println("PDF TEXT");
+        System.out.println("=================================");
+        System.out.println(text);
+
+        return ResponseEntity.ok("PDF Read Successfully");
     }
-
 }
